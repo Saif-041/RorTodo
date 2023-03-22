d86c8c82bf82b8280af5b1cc0091174822060872
Saif-041 <saif786999999@gmail.com>
2023-03-22 20:42:25 +0500

    adding user model and db

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_21_074344) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_22_153635) do
   create_table "todos", force: :cascade do |t|
     t.string "name"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
